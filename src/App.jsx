7f0f8e412b059a97cc1051ca894ffc9ad9353377
mattpsvreis <mateuspsvreis@gmail.com>
2023-04-25 12:29:39 -0300

    Checkpoint Lecture 53: Finalizing Touches for Monsters Rolodex

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ export default class App extends Component {
 
     return (
       <div className='App'>
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox
           className='monsters-search-box'
           placeholder='search monsters'
